5730bf38a74a339b26edf6eab82f8a793c70bb97
Behpakhsh It Department <behpakhsh.it@gmail.com>
2020-09-13 13:13:02 +0430

    add baseAdapter

---
+++ recyclerutilities/src/main/java/bpc/dis/recyclerutilities/DisBaseAdapter.java
@@ -0,0 +1,163 @@
+package bpc.dis.recyclerutilities;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import bpc.dis.utilities.ResourceManager.ResourceManagerListener;
+
+public abstract class DisBaseAdapter<T> extends RecyclerView.Adapter<DisBaseViewHolder<T>> implements ResourceManagerListener {
+
+    private int viewCount = 0;
+    private List<T> data;
+    private Context context;
+
+    protected DisBaseAdapter(Context context, List<T> data) {
+        this.data = data;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public DisBaseViewHolder<T> onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return onCreateBaseViewHolder(parent, viewType);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DisBaseViewHolder holder, int position) {
+        if (position < 0) {
+            return;
+        }
+        if (position > 0 && position < viewCount - 1 && position - 1 < data.size()) {
+            holder.setData(data.get(position - 1));
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return super.getItemViewType(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    @Override
+    public void onDetachedFromRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView);
+    }
+
+    @Override
+    public Drawable getDrawable(int resId) {
+        return getContext().getResources().getDrawable(resId);
+    }
+
+    @Override
+    public int getColor(int resId) {
+        return getContext().getResources().getColor(resId);
+    }
+
+    @Override
+    public float getDimension(int resId) {
+        return getContext().getResources().getDimension(resId);
+    }
+
+    @Override
+    public String getString(int resId) {
+        return getContext().getResources().getString(resId);
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void add(T object) {
+        if (object != null) {
+            data.add(object);
+            int position = viewCount - 1;
+            viewCount++;
+            notifyItemInserted(position);
+        }
+    }
+
+    public void insert(T object, int itemPosition) {
+        int maxPosition = viewCount - 1;
+        if (data != null && itemPosition < maxPosition && object != null) {
+            data.add(itemPosition, object);
+            viewCount++;
+            notifyItemInserted(itemPosition);
+        }
+    }
+
+    private void addAll(List<T> data) {
+        if (data == null) {
+            return;
+        }
+        int size = data.size();
+        if (size > 0) {
+            this.data.addAll(data);
+            int positionStart = viewCount;
+            viewCount += size;
+            notifyItemRangeInserted(positionStart, size);
+        }
+    }
+
+    public void addAll(T[] objects) {
+        addAll(Arrays.asList(objects));
+    }
+
+    public void replace(T object, int itemPosition) {
+        if (data != null && object != null) {
+            if (itemPosition < data.size()) {
+                data.set(itemPosition, object);
+                viewCount++;
+                notifyItemChanged(itemPosition);
+            }
+        }
+    }
+
+    public void remove(T object) {
+        if (object != null && !data.contains(object)) {
+            return;
+        }
+        int dataPosition = data.indexOf(object);
+        remove(dataPosition);
+    }
+
+    private void remove(int itemPosition) {
+        if (itemPosition >= data.size()) {
+            throw new IllegalArgumentException("itemPosition is greater than data size");
+        } else {
+            data.remove(itemPosition);
+            notifyItemRemoved(itemPosition);
+            viewCount--;
+        }
+    }
+
+    public void clear() {
+        data.clear();
+        notifyDataSetChanged();
+    }
+
+    protected List<T> getData() {
+        return data;
+    }
+
+    protected T getData(int position) {
+        return data.get(position);
+    }
+
+    protected abstract DisBaseViewHolder<T> onCreateBaseViewHolder(ViewGroup parent, int viewType);
+
+    public int getCount() {
+        return viewCount;
+    }
+
+}
---
+++ recyclerutilities/src/main/java/bpc/dis/recyclerutilities/DisBaseViewHolder.java
@@ -0,0 +1,31 @@
+package bpc.dis.recyclerutilities;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public abstract class DisBaseViewHolder<T> extends RecyclerView.ViewHolder {
+
+    private T data;
+
+    public DisBaseViewHolder(View itemView) {
+        super(itemView);
+        onInitializeView();
+    }
+
+    private void onInitializeView() {
+
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    void setData(final T data) {
+        if (data == null) {
+            return;
+        }
+        this.data = data;
+    }
+
+}
